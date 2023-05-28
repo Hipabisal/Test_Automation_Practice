5fcc38eab737c3534a0341d21b8175b5649285e0
Adrian Klysinski <adrian.klysinski@gmail.com>
2023-05-28 16:40:40 +0200

    4th and 5th tests

--- src/test/java/UltimateQATest.java
+++ src/test/java/UltimateQATest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class UltimateQATest extends PageSetup {
 
    @Test
@@ -18,6 +21,7 @@ public class UltimateQATest extends PageSetup {
 
    @Test
     public void testTwo() throws InterruptedException {
+
       WebElement nameTextField = driver.findElementByName("et_pb_contact_name_0");
       nameTextField.sendKeys("Tester");
 
@@ -33,7 +37,41 @@ public class UltimateQATest extends PageSetup {
 
      WebElement thanksText = driver.findElementByClassName("et-pb-contact-message");
      Assertions.assertEquals("Thanks for contacting us", thanksText.getText());
-
    }
+
+   @Test
+    public void testThree(){
+
+       WebElement blueField = driver.findElementByXPath("//*[contains(@class, 'et_pb_module et_pb_cta_0 et_pb_promo')]");
+       Assertions.assertEquals("rgba(46, 163, 242, 1)", blueField.getCssValue("background-color"));
+   }
+
+    @Test
+    public void testFour() {
+       WebElement bikeCheckbox = driver.findElementByXPath("//[@type= 'checkbox' and value = 'Bike']");
+       bikeCheckbox.click();
+       Assertions.assertTrue(bikeCheckbox.isSelected());
+    }
+
+    @Test
+    public void testFive(){
+
+       List<String> listOfCars = new ArrayList<>();
+       listOfCars.add("volvo");
+       listOfCars.add("Saab");
+       listOfCars.add("Opel");
+       listOfCars.add("Audi");
+
+
+       for (int i = 0; i < listOfCars.size(); i++) {
+
+           WebElement dropdownOption = driver.findElementByXPath("//*[@value='saab']");
+             dropdownOption.click();
+             Assertions.assertTrue(dropdownOption.isSelected());
+
+       }
+
+    }
+
 
 }
