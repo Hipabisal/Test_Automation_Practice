5c72724d5686eaa32d01b482c4bcd4c2825e72fe
Adrian Klysinski <adrian.klysinski@gmail.com>
2023-05-28 14:04:06 +0200

    First Test implemented

--- src/test/java/UltimateQATest.java
+++ src/test/java/UltimateQATest.java
@@ -1,16 +1,22 @@
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class UltimateQATest extends PageSetup {
 
    @Test
   public void testOne() {
+       WebElement buttonUsingId = driver.findElementById( "idExample");
+        buttonUsingId.click();
+
+        WebElement buttonSuccessText = driver.findElementByClassName("entry-title");
+        Assertions.assertEquals("Button success", buttonSuccessText.getText());
 
 
-
-    }
+   }
 
 
 }
